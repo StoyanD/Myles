339f942461793166e87b6462059d2874d47db14d
Stoyan Dimitrov <s.dimitrov19@gmail.com>
2015-10-24 13:43:31 -0700

    removed gen activity and added intro act and frag, and material colors

---
+++ app/src/main/java/com/hack/twenty/activities/IntroActivity.java
@@ -0,0 +1,19 @@
+package com.hack.twenty.activities;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+/**
+ * Created by stoyan on 10/24/15.
+ */
+public class IntroActivity extends FragmentActivity {
+
+    @Override
+    protected void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+
+//        mLoginFragment = (AbstractFragment) getFragmentManager().findFragmentById(R.id.fragment_login);
+    }
+
+}
---
+++ app/src/main/java/com/hack/twenty/fragments/IntroFragment.java
@@ -0,0 +1,9 @@
+package com.hack.twenty.fragments;
+
+import android.app.Fragment;
+
+/**
+ * Created by stoyan on 10/24/15.
+ */
+public class IntroFragment extends Fragment {
+}
