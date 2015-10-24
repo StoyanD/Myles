773a684a4bbf7a56c51d29960b1c4578b1212c78
Stoyan Dimitrov <s.dimitrov19@gmail.com>
2015-10-24 13:57:08 -0700

    added databinding, moved view to intro frag

--- app/src/main/java/com/hack/twenty/activities/IntroActivity.java
+++ app/src/main/java/com/hack/twenty/activities/IntroActivity.java
@@ -3,6 +3,8 @@ package com.hack.twenty.activities;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 
+import com.hack.twenty.R;
+
 /**
  * Created by stoyan on 10/24/15.
  */
--- app/src/main/java/com/hack/twenty/fragments/IntroFragment.java
+++ app/src/main/java/com/hack/twenty/fragments/IntroFragment.java
@@ -1,9 +1,39 @@
 package com.hack.twenty.fragments;
 
 import android.app.Fragment;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.hack.twenty.databinding.FragIntroBinding;
 
 /**
  * Created by stoyan on 10/24/15.
  */
 public class IntroFragment extends Fragment {
+    /**
+     * TAG for logging
+     */
+    private static final String TAG = "IntroFragment";
+    /**
+     * The layout binding for this fragment
+     */
+    private FragIntroBinding mBinding;
+
+    @Override
+    public View onCreateView(final LayoutInflater inflater, final ViewGroup container, final Bundle savedInstanceState) {
+        mBinding =  FragIntroBinding.inflate(inflater, container, false);
+//        mBinding.setLoginFrag(this);
+//        mBinding.password.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+//            @Override
+//            public boolean onEditorAction(TextView view, int actionId, KeyEvent event) {
+//                return editorActionPassword(view, actionId, event);
+//            }
+//        });
+        return mBinding.getRoot();
+    }
+
 }
